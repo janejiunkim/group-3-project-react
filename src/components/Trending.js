c98b3371d139c7446a8f8207ce833bb46929d80c
CalClemmer <cal1296@yahoo.com>
2021-10-02 10:58:53 -0700

    Fix: Now correctly displays API data

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,33 +1,50 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Game from './Game';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Game from "./Game";
+import GameCard from "./GameCard";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Trending = () => {
-    const [trendingGames, setGames] = useState([]);
+  const [trendingGames, setGames] = useState([]);
 
-    let url = process.env.REACT_APP_API + 'videogames/trending/';
+  //   let url = process.env.REACT_APP_SERVER_URL + "videogames/trending/"; //process.env.REACT_APP_API is undefined
+  let url = "videogames/trending/";
 
-    useEffect(() => {
-        axios.get(url)
-        .then(response => {
-            setGames(response.data);
-            console.log('INSIDE USE EFFECT', trendingGames);
-        });
-    }, []);
+  useEffect(() => {
+    axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
+    axios.get(url).then((response) => {
+      setGames(response.data.game);
+      console.log("Testing trending", response.data.game);
+    });
+  }, []);
 
-    console.log('STATE', trendingGames);
+  console.log("STATE", trendingGames);
 
-    
-    return (
-        <div className="trending">
-            <Row className="mx-0">
-                <Game as={Col}></Game>
-            </Row>
+  // this makes sure that the data is loaded before passing it through
+  function trendingList(n) {
+    let gameArr = [];
+    for (let i = 0; i < n; i++) {
+      gameArr.push(
+        <Col>
+          <GameCard gameData={trendingGames[i]}></GameCard>
+        </Col>
+      );
+    }
+    return gameArr;
+  }
 
-        </div>
-    )
-}
+  let displayTrendingList = trendingGames.length ? (
+    trendingList(6)
+  ) : (
+    <h2>Loading...</h2>
+  );
+
+  return (
+    <div className="trending">
+      <Row className="mx-0">{displayTrendingList}</Row>
+    </div>
+  );
+};
 
 export default Trending;
