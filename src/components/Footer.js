2366ebcc7929174821fb124ce623cf37f96b5b4b
CalClemmer <cal1296@yahoo.com>
2021-10-04 16:04:19 -0700

    Game Page Summary, Scores, and Trailer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className="footer bg-dark">
-            <div className="container text-center">
-                <span className="text-muted">@2021 Built by Kids-R-Us</span>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer bg-dark">
+      <div className="container text-center">
+        <span className="text-muted">@2021 Built by Kids-R-Us</span>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
